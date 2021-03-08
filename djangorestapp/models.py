52e2215adce1d502a35c38169fb873a23d10b9f0
ZiaurJoy <ziaurjoy802@gmail.com>
2021-03-08 21:19:04 +0600

    Normal Serializer

--- djangorestapp/models.py
+++ djangorestapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    auther = models.CharField(max_length=50)
+    email = models.EmailField()
+    date_time = models.DateTimeField(auto_now_add=True)
+
+    def __str(self):
+        return self.title
+
---
+++ djangorestapp/serializer.py
@@ -0,0 +1,22 @@
+from djangorestapp.models import Article
+
+from rest_framework import serializers
+
+class ArticleSirializer(serializers.Serializer):
+    title = serializers.CharField(max_length=100)
+    auther = serializers.CharField(max_length=50)
+    email = serializers.EmailField()
+    date_time = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.auther = validated_data.get('auther', instance.auther)
+        instance.email = validated_data.get('email', instance.email)
+        instance.date_time = validated_data.get('date_time', instance.date_time)
+        instance.save()
+        return instance
+
+    
